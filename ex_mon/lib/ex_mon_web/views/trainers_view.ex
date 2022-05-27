29f1b06e37f19d8e68313b1eaef46b0977ad3174
Diego Araújo <diesaraujo@gmail.com>
2022-05-27 18:20:29 -0300

    Finalizando o Update do trainer

--- ex_mon/lib/ex_mon_web/views/trainers_view.ex
+++ ex_mon/lib/ex_mon_web/views/trainers_view.ex
@@ -12,6 +12,18 @@ defmodule ExMonWeb.TrainersView do
     }
   end
 
+  def render("update.json", %{trainer: trainer}) do
+    %{
+      message: "Trainer updated",
+      trainer: %{
+        id: trainer.id,
+        name: trainer.name,
+        inserted_at: trainer.inserted_at,
+        updated_at: trainer.updated_at
+      }
+    }
+  end
+
   def render("show.json", %{trainer: trainer}) do
     %{
       id: trainer.id,
